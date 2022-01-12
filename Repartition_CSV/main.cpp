9236ae4aa263c2df3fc61a24cc0f80938bb8374d
Marine BRIOUDE <m.brioude@quadria.fr>
2022-01-12 10:13:29 +0100

    final

--- Repartition_CSV/main.cpp
+++ Repartition_CSV/main.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main()
 {
+    SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED);
+
     MYSQL connexion;
     mysql_init(&connexion);
 
-    if (mysql_real_connect(&connexion, "192.168.1.101", "admin", "root", "mix", 0, NULL, 0))
+    if (mysql_real_connect(&connexion, "192.168.1.150", "admin", "root", "vCloud_CSV", 0, NULL, 0))
     {
-        const char* chemin = "C:\\Resourcesv2\\juin - decembre\\septembre - decembre"; // chemin absolu de recherche de rapports
+        const char* chemin = "C:\\Resourcesv2"; // chemin absolu de recherche de rapports
         exploration(&connexion, chemin);
 
         mysql_close(&connexion); //fermeture connection
@@ -18,5 +20,8 @@ int main()
     {
         fprintf(stderr, "Failed to connect to database: Error: %s\n", mysql_error(&connexion));
     }
+
+    SetThreadExecutionState(ES_CONTINUOUS);
+
     return(0);
 }
